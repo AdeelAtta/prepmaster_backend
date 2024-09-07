97ef3d2d5b11ce613dc3dc8a01ba7bb442a61fcb
adeel-atta <adeelatta2000@gmail.com>
2024-09-07 14:57:38 +0500

    cors added

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var authRouter = require(`./routes/auth`);
 var app = express();
 
 var corsOptions = {
-  origin: [`http://localhost:8080`,`http://localhost:8080/`,'https://prep-master-client.vercel.app'],
+  origin: [`http://localhost:8080`,`http://localhost:8080/`,'https://prep-master-client.vercel.app','https://prep-master-client.vercel.app/'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
